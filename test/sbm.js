edb8247a96f4a2b5f6d7541b499c17fdde23c7fd
Matthieu Freeman <me@ilambda.io>
2016-03-07 07:11:55 +0100

    Edited library

--- test/sbm.js
+++ test/sbm.js
@@ -9,8 +9,13 @@ var sbm = require('../lib/sbm.js')
 0 1
 */
 
-var mat = sbm.make([[1, 1, 0], [1, 0, 1]])
+var n = 3
+var id = sbm.make(function(x, y) { return x == y }, {x: n, y: n})
+var mat = sbm.make(function (i, j) { return (i + j) % 2 === 0 }, {x: n, y: n})
+var mat2 = sbm.make([[1, 1, 0], [1, 0, 1], [0, 1, 1]])
+
 console.log(sbm.matrix(mat))
-console.log(mat.data)
-console.log(sbm.transpose(mat).data)
-console.log(sbm.density(sbm.transpose(mat)))
+console.log(sbm.matrix(sbm.multiply(id, mat)))
+console.log(" ")
+console.log(sbm.matrix(mat2))
+console.log(sbm.matrix(sbm.multiply(id, mat2)))
